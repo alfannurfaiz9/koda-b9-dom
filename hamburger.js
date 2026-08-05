a0a3e150adc1b05a51590ae20a33a952a67215f7
alfan <alfannurfaizwork@gmail.com>
2026-08-05 03:18:12 -0500

    refactoring: refactor from manual cheking to toggle

--- hamburger.js
+++ hamburger.js
@@ -2,12 +2,5 @@ const hamburger = document.querySelector("#hamburger-menu");
 const ul = document.querySelector("header>nav>ul");
 
 hamburger.addEventListener("click", () => {
-  if (!ul.classList.value) {
-    hamburger.setAttribute("src", "./assets/close.svg");
-    ul.classList.add("show");
-    return;
-  }
-
-  hamburger.setAttribute("src", "./assets/hamburger.svg");
-  ul.classList.remove("show");
+  ul.classList.toggle("show");
 });
